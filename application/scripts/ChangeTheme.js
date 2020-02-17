89e893a7a25228fa247e3ef9d0413ba48f913547
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-02-17 12:46:14 +0000

    Make theme persist across multiple pages

--- application/scripts/ChangeTheme.js
+++ application/scripts/ChangeTheme.js
@@ -1,7 +1,21 @@
 //custom video controls v1
+
+if (localStorage.getItem("theme") === null) {
+    localStorage.setItem("theme", "dark");
+}
+
 document.addEventListener("DOMContentLoaded", initialiseWebPage)
 
 function initialiseWebPage() {
+
+    var getUrl = window.location;
+    var baseURL = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+
+    if (localStorage.getItem("theme") === "dark") {
+        document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/DarkStyle.css");
+    } else {
+        document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/LightStyle.css");
+    }
 
     const changeStyleButton = document.getElementById("changeStyleButton");
     changeStyleButton.addEventListener("click", swapStyleSheet);
@@ -10,13 +24,12 @@ function initialiseWebPage() {
 
         console.log("Change theme button clicked");
 
-        var getUrl = window.location;
-        var baseURL = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-
         if (document.getElementById("pagestyle").getAttribute("href") === baseURL + "/application/css/LightStyle.css") {
             document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/DarkStyle.css");
+            localStorage.setItem("theme", "dark");
         } else {
             document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/LightStyle.css");
+            localStorage.setItem("theme", "light");
         }
     }
 }
--- application/views/home.php
+++ application/views/home.php
@@ -25,7 +25,7 @@
     <title><?php echo $title ?></title>
 </head>
 
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark rounded">
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark rounded" id="navbar">
     <span class="navbar-brand">1CKW50</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
