1f2dce48a5a7e77c657d3163a9d7fb24d5b546aa
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-03-01 18:18:48 +0000

    Display light mmu logo if light theme selected

--- application/scripts/ChangeTheme.js
+++ application/scripts/ChangeTheme.js
@@ -15,8 +15,10 @@ function initialiseWebPage() {
 	// set theme based upon local storage:
 	if (localStorage.getItem("theme") === "dark") {
 		document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/DarkStyle.css");
+		document.getElementById("mmuLogo").setAttribute("src", baseURL + "/application/images/mmuDark.jpg");
 	} else {
 		document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/LightStyle.css");
+		document.getElementById("mmuLogo").setAttribute("src", baseURL + "/application/images/mmuLight.png");
 	}
 
 	const changeThemeButton = document.getElementById("changeThemeButton");
@@ -29,9 +31,11 @@ function initialiseWebPage() {
 
 		if (document.getElementById("pagestyle").getAttribute("href") === baseURL + "/application/css/LightStyle.css") {
 			document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/DarkStyle.css");
+			document.getElementById("mmuLogo").setAttribute("src", baseURL + "/application/images/mmuDark.jpg");
 			localStorage.setItem("theme", "dark");
 		} else {
 			document.getElementById("pagestyle").setAttribute("href", baseURL + "/application/css/LightStyle.css");
+			document.getElementById("mmuLogo").setAttribute("src", baseURL + "/application/images/mmuLight.png");
 			localStorage.setItem("theme", "light");
 		}
 	}
