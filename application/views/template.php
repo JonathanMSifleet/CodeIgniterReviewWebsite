cd803069984004cd5a2383b7da896139820540b4
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-03-02 22:33:28 +0000

    Display warning message if not using chrome

--- application/views/template.php
+++ application/views/template.php
@@ -1,3 +1,14 @@
+<script>
+
+	var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+	if (localStorage.getItem("wrongBrowserMessageDisplayed") === null && !isChrome) {
+		window.alert("Pages may not load correctly, please use chrome for optimal experience")
+		localStorage.setItem("wrongBrowserMessageDisplayed", "true");
+	}
+
+</script>
+
 <?php
 $this->load->view('templates/header', $page);
 $this->load->view($bodyContent);
