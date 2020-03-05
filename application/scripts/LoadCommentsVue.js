041280c2d5e017f917c508080a2cc26b1ede97a3
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-03-05 18:34:02 +0000

    Post comment using Vue

--- application/scripts/LoadCommentsVue.js
+++ application/scripts/LoadCommentsVue.js
@@ -1,30 +1,25 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    var app = new Vue({
-        el: '#commentsSection',
-        data: {
-            comments: []
-        },
-        created: function () {
-            console.log("Created");
-        },
-        methods: {
-            setComment: function () {
+	var app = new Vue({
+		el: '#commentsSection',
+		data: {
+			comments: []
+		},
+		methods: {
+			setComment: function () {
 
-                console.log("Click");
+				console.log("Click");
 
-                var reviewID = $("#reviewID").val();
-                var timeStamp = $("#timeStamp").val();
-                var comment = $("#commentInput").val();
+				var reviewID = $("#reviewID").val();
+				var timeStamp = $("#timeStamp").val();
+				var comment = $("#commentInput").val();
 
-                var postData = {"reviewID": reviewID, "timeStamp": timeStamp, "comment": comment};
+				var postData = {"reviewID": reviewID, "timeStamp": timeStamp, "comment": comment};
 
-                console.log(json.stringify(postData));
-
-                $.post("http://localhost:82/games-review/SetComment", postData)
-                    .done(function (postData) {
-                        app.comments.push(postData);
-                    });
-            }
-        }
-    })
+				$.post("http://localhost:82/games-review/SetComment", postData)
+					.done(function (postData) {
+						app.comments.push(postData);
+					});
+			}
+		}
+	})
 });
--- application/views/review.php
+++ application/views/review.php
@@ -2,28 +2,28 @@
 
 <!-- display review title from database -->
 <title><?php
-    foreach ($gameReview as $review) {
-        echo $review->GameName . " Review";
-        break;
-    } ?>
+	foreach ($gameReview as $review) {
+		echo $review->GameName . " Review";
+		break;
+	} ?>
 </title>
 
 <body>
 <div id="review">
 
-    <?php
-    // load review data from database:
-    foreach ($gameReview as $review) {
-        $gameName = $review->GameName;
-        $gameBlurb = $review->GameBlurb;
-        $gameReview = $review->GameReview;
-        $gameImage = $review->ReviewImage;
-        $_SESSION['gameComments'] = $review->GameComments;
-        $_SESSION['reviewID'] = $review->ID;
-        $imagePath = base_url() . "application/images/" . $gameImage;
+	<?php
+	// load review data from database:
+	foreach ($gameReview as $review) {
+		$gameName = $review->GameName;
+		$gameBlurb = $review->GameBlurb;
+		$gameReview = $review->GameReview;
+		$gameImage = $review->ReviewImage;
+		$_SESSION['gameComments'] = $review->GameComments;
+		$_SESSION['reviewID'] = $review->ID;
+		$imagePath = base_url() . "application/images/" . $gameImage;
 
-        // display jump to comments button
-        echo <<<_END
+		// display jump to comments button
+		echo <<<_END
 		<div clas="reviewTitle">
 			<button type="button" class="btn btn-light" id="jumpToComments">Jump to comments</button>
 			<h1>$gameName</h1>	
@@ -45,46 +45,51 @@
 			</div>
 		</div>
 _END;
-        break;
-    }
-    ?>
+		break;
+	}
+	?>
 
 </div>
 
 <div class="commentsContainerOuter rounded" id="commentsContainer">
-    <div class="commentsContainerInner rounded">
-        <div id="commentsSection">
-            <?php
-            // if game comments are disabled hide comments div and display appropriate message
-            if ($_SESSION['gameComments'] == 0) {
-                echo "<h3 id='commentsDisabledMessage' class='rounded'>Comments are disabled for this review </h3>";
-            } else {
-                // if the user is not logged in display appropriate message
-                if (!isset($_SESSION['loggedIn'])) {
-                    echo "<p id='notLoggedIn' class='rounded'>You must be logged in to post a comment</p>";
-                } elseif (isset($_SESSION['loggedIn'])) {
-                    // if the user is not logged in display appropriate message
-                    if (!$_SESSION['loggedIn']) {
-                        echo "<p id='notLoggedIn' class='rounded'>You must be logged in to post a comment</p>";
-                    } else {
-                        $username = $_SESSION['loggedInUsername'];
-                        $currentURL = $_SERVER['REQUEST_URI'];
-                        $curDate = date("Y-m-d H:i:s", time());
-                        // if the user is logged in display comment form
-                        echo <<<_END
+	<div class="commentsContainerInner rounded">
+		<div id="commentsSection">
+			<?php
+			// if game comments are disabled hide comments div and display appropriate message
+			if ($_SESSION['gameComments'] == 0) {
+				echo "<h3 id='commentsDisabledMessage' class='rounded'>Comments are disabled for this review </h3>";
+			} else {
+				// if the user is not logged in display appropriate message
+				if (!isset($_SESSION['loggedIn'])) {
+					echo "<p id='notLoggedIn' class='rounded'>You must be logged in to post a comment</p>";
+				} elseif (isset($_SESSION['loggedIn'])) {
+					// if the user is not logged in display appropriate message
+					if (!$_SESSION['loggedIn']) {
+						echo "<p id='notLoggedIn' class='rounded'>You must be logged in to post a comment</p>";
+					} else {
+						$username = $_SESSION['loggedInUsername'];
+						$currentURL = $_SERVER['REQUEST_URI'];
+						$curDate = date("Y-m-d H:i:s", time());
+						// if the user is logged in display comment form
+						echo <<<_END
                 <input type="text" name="commentInput" class="form-control rounded" id="commentInput"
                        placeholder="Enter comment">
                 <input type="hidden" name="reviewID" id="reviewID" value="{$_SESSION['reviewID']}">
                 <input type="hidden" name="timeStamp" id="timeStamp" value="$curDate">
                 <button v-on:click="setComment()" id="submitCommentButton" name="submitCommentButton" class="btn btn-primary">Submit</button>
 _END;
-                    }
-                }
-            }
+					}
+				}
+			}
 
-            ?>
-        </div>
-    </div>
+			?>
+			<!--
+			<div id="vueComments" class="rounded" v-for="comment in comments">
+				<p>{{comment[0].UserComment}}</p>
+				<p>{{comment.TimeStamp}}</p>
+			</div> -->
+		</div>
+	</div>
 </div>
 
 </body>
